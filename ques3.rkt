bc80f0bc2808b4a4b903b735c7ea5088edcf7cec
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-27 01:44:13 -0500

    Added main condition

--- ques3.rkt
+++ ques3.rkt
@@ -1,7 +1,8 @@
 (DEFINE (sum-up-numbers-general L)
         (COND
             ((NULL? L) 0)
-            ((NUMBER? L) L) 
+            ((NUMBER? L) L)
+            ((LIST? L) (+ (sum-up-numbers-general (CAR L)) (sum-up-numbers-general (CDR L))))
             (ELSE (sum-up-numbers-general (CDR '(L))))
         )
 )
