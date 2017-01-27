12f204f886752aa9ffeef5c339c1bb242073686a
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-27 01:45:41 -0500

    Test cases with output

--- ques3.rkt
+++ ques3.rkt
@@ -7,3 +7,26 @@
         )
 )
 
+(sum-up-numbers-general '())
+(sum-up-numbers-general '(() 100 a 200))
+(sum-up-numbers-general '(100))
+(sum-up-numbers-general '(100 200))
+(sum-up-numbers-general '(a))
+(sum-up-numbers-general '(a 100 b 200 c 300 d))
+(sum-up-numbers-general '(()))
+(sum-up-numbers-general '(100))
+(sum-up-numbers-general '(100 (200)))
+(sum-up-numbers-general '(a 100 b (200) c 300 d))
+(sum-up-numbers-general '(a 100 ((b((200)c)) 300 d)))
+
+; 0
+; 300
+; 100
+; 300
+; 0
+; 600
+; 0
+; 100
+; 300
+; 600
+; 600
