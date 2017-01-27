29774987b923a7ba63d23a4b49c819553b6096ad
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-27 00:31:34 -0500

    Added conditions

--- ques2.rkt
+++ ques2.rkt
@@ -1,6 +1,10 @@
 (DEFINE (sum-up-numbers-simple L)
-        
+        (COND
+             ((NULL? '()) 0)
+             ((LIST? (CAR L)) (sum-up-numbers-simple (CDR L)))
+             
         )
+)
 
 (DEFINE (helper L)
 
