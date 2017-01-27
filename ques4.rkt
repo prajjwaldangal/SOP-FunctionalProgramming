d27b74fb2deb35ba1758e1086dab46da279a15d2
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-27 14:08:46 -0500

    modification with min function

--- ques4.rkt
+++ ques4.rkt
@@ -1,5 +1,17 @@
 (DEFINE (min-above-min L1 L2)
         (COND
-            (NULL? L2) (
+             ((NULL? L2) (smallest L1 0))
+             (ELSE ) 
+        )
+ )
+
+
+(DEFINE (smallest L num)
+        (COND
+             ; ((NULL? L) 
+             ((< CAR(L) (CAR (CDR (L)))) (smallest ((append (CAR (L)) (CDR (CDR (L)))))))
+             ; (ELSE
         )
 )
+
+(smallest '(10 22 15 19 1))
