85865693dd22ec06b842eec22ccabde2fa980029
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-27 00:45:42 -0500

    Tests along with the results

--- ques2.rkt
+++ ques2.rkt
@@ -1,10 +1,31 @@
 (DEFINE (sum-up-numbers-simple L)
         (COND
-             ((NULL? '()) 0)
+             ((NULL? L) 0)
              ((LIST? (CAR L)) (sum-up-numbers-simple (CDR L)))
-             ((number? (CAR L)) (+ (CAR L) (sum-up-number-simple (CDR L))))
-             (ELSE (sum-up-number-simple (CDR L)))
+             ((number? (CAR L)) (+ (CAR L) (sum-up-numbers-simple (CDR L))))
+             (ELSE (sum-up-numbers-simple (CDR L)))
         )
 )
 
+
+(sum-up-numbers-simple '())
+(sum-up-numbers-simple '(100 200))
+(sum-up-numbers-simple '(a b c))
+(sum-up-numbers-simple '(100 a))
+(sum-up-numbers-simple '(a 100))
 (sum-up-numbers-simple '(a 100 b 200 c 300 d))
+(sum-up-numbers-simple '( () ))
+(sum-up-numbers-simple '((100)))
+(sum-up-numbers-simple '(100 (200)))
+(sum-up-numbers-simple '(a 100 b (200) c 300 d))
+
+;0
+;300
+;0
+;100
+;100
+;600
+;0
+;0
+;100
+;400
