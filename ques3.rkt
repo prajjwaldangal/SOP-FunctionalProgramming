55e8f3b90f7f48d70e1a0b7075c1e27144c3b322
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-27 01:37:04 -0500

    primitive conditions added

--- ques3.rkt
+++ ques3.rkt
@@ -1,8 +1,9 @@
 (DEFINE (sum-up-numbers-general L)
         (COND
             ((NULL? L) 0)
-            ((NUMBER? L) (+ L (CDR 'L))) 
-            ((LIST? L) (+
-            (ELSE (sum-up-numbers-general (CDR L)))
+            ((NUMBER? L) L) 
+            
+            
         )
 )
+
