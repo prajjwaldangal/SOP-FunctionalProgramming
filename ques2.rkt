60a297faf4294545e9a461fa37c6cc826ff3320c
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-27 00:32:13 -0500

    Added another condition

--- ques2.rkt
+++ ques2.rkt
@@ -2,7 +2,7 @@
         (COND
              ((NULL? '()) 0)
              ((LIST? (CAR L)) (sum-up-numbers-simple (CDR L)))
-             
+             ((number? (CAR L)) (+ (CAR L) (sum-up-number-simple (CDR L))))
         )
 )
 
