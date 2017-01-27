21a7fa64b17ee597b14490b5b1b4d00c256f2748
Prajjwal Dangal <prajjwal.dangal@bison.howard.edu>
2017-01-27 00:35:59 -0500

    trial with cases

--- ques2.rkt
+++ ques2.rkt
@@ -3,5 +3,8 @@
              ((NULL? '()) 0)
              ((LIST? (CAR L)) (sum-up-numbers-simple (CDR L)))
              ((number? (CAR L)) (+ (CAR L) (sum-up-number-simple (CDR L))))
+             (ELSE (sum-up-number-simple (CDR L)))
         )
 )
+
+(sum-up-numbers-simple '(a 100 b 200 c 300 d))
